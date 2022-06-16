7db19c76a897ace6396bfdb9a88be7e9a29be4a9
Jaqueline Moura <jaquelinemoura402@gmail.com>
2022-06-16 16:25:04 -0300

    component sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Header } from "./components/Header";
-import styles from "./App.module.css";
+
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export default function App() {
   return (
     <>
       <Header />
       <div className={styles.wrapper}>
-        <aside>side bar</aside>
+        <Sidebar />
         <main>Post</main>
       </div>
     </>
